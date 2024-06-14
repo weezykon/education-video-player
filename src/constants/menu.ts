44ce3f277b919a4d3d386b3aa8e03712b0bf7202
Akinbode Akinbode <weezykon@gmail.com>
2024-06-14 07:34:18 +0100

    chore: add layout template

---
+++ src/constants/menu.ts
@@ -0,0 +1,42 @@
+export const MENU_ITEMS = [
+  {
+    name: 'Arts & Humanities',
+    link: '#',
+    slug: 'arts-humanities'
+  },
+  {
+    name: 'Business',
+    link: '#',
+    slug: 'business'
+  },
+  {
+    name: 'Computer Science',
+    link: '#',
+    slug: 'computer-science'
+  },
+  {
+    name: 'Data Science',
+    link: '#',
+    slug: 'data-science'
+  },
+  {
+    name: 'Information Technology',
+    link: '#',
+    slug: 'information-technology'
+  },
+  {
+    name: 'Health',
+    link: '#',
+    slug: 'health'
+  },
+  {
+    name: 'Math & Logic',
+    link: '#',
+    slug: 'math-logic'
+  },
+  {
+    name: 'Personal Development',
+    link: '#',
+    slug: 'personal-development'
+  }
+];
---
+++ src/ui/templates/layout/Layout.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Paragraph } from 'ui/atoms/paragraph'
+import { LayoutProps } from './types';
+import { Heading } from 'ui/atoms/heading';
+import { MENU_ITEMS } from 'constants/menu';
+import { NavLink } from 'react-router-dom';
+
+const Layout = (props: LayoutProps) => {
+  return (
+    <div className="flex flex-col min-h-screen w-full">
+      <header className="w-full flex justify-between items-center h-[80px] xl:h-[100px] px-6">
+        {/* logo */}
+        <div className="flex items-center">
+          <Paragraph size="b3" className="tracking-[10px]" color="text-gray-400">
+            EDU
+          </Paragraph>
+        </div>
+      </header>
+      <main className="flex-grow">
+        {/* Add main content here */}
+        <div className='w-full flex'>
+          <div className='flex flex-col w-3/12 py-12 px-8 space-y-7'>
+            <Heading
+              variant='h1'
+              size='h6'
+              weight="bold"
+              className=""
+            >
+              Explore
+            </Heading>
+
+            <nav className="w-full">
+              <ul className="w-full flex flex-col justify-start items-start space-y-4">
+                {Array.isArray(MENU_ITEMS) && MENU_ITEMS.length > 0 ? MENU_ITEMS.map((menu) => {
+                  return (
+                    <li key={menu?.slug} className="w-full">
+                      <NavLink to={menu?.link} className="text-gray-800 hover:text-gray-800">
+                        {menu?.name}
+                      </NavLink>
+                    </li>
+                  )
+                }) : null}
+              </ul>
+            </nav>
+          </div>
+          <div className='w-9/12 h-[calc(100vh_-_80px)] xl:h-[calc(100vh_-_100px)] scroll-smooth overflow-y-scroll'>
+            {props.children}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Layout
